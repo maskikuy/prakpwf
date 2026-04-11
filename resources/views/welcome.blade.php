aae03b31ed9319a66d0a1cf004288f8559428540
maskikuy <kproject305@gmail.com>
2026-04-11 11:54:53 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tugas Pertemuan 1</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-[#0f0f0f] text-white flex items-center justify-center min-h-screen font-sans antialiased">
+
+    <div class="border border-gray-800 rounded-lg p-10 w-full max-w-4xl h-72 flex flex-col justify-center mx-4">
+        
+        <h1 class="text-xl font-medium tracking-wide">Rizky Gusti Afiq</h1>
+        
+        <p class="text-gray-400 mt-2 mb-6 text-sm">20230140060</p> 
+
+        <div>
+            <a href="#" class="inline-block bg-[#f4f4f5] text-black font-medium px-4 py-2 rounded hover:bg-gray-300 transition duration-200">
+                Modul Pertemuan 1
+            </a>
+        </div>
+
+    </div>
+
+</body>
+</html>
